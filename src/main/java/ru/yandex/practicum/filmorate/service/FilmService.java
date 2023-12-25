3fdef22704f271a299ae0147ce8c586c3d88d60b
Anna Vashchenkova <anna.inkognito7@yandex.ru>
2023-12-26 01:34:33 +0300

    Checkstyle 2 -------

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -15,7 +15,6 @@ import ru.yandex.practicum.filmorate.exception.SqlException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.User;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
